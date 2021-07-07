e582caf4d7a024619e08cdf736d29769a4f7262b
Viktor Ser <datakobs0@gmail.com>
2021-07-07 14:39:31 +0200

    reconciliation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import newReact from './newReact'
 
 /** @jsx newReact.createElement */
-const testElement = (
-  <div>
-    <h1>This is my own implementation of React</h1>
-    <input type="text" />
-    <p>This is another text</p>
-  </div>
-)
 
 const rootContainer = document.getElementById('root')
 
-newReact.render(testElement, rootContainer)
+const updateValue = (e) => {
+  rerender(e.target.value)
+}
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <h1>This is my own implementation of React</h1>
+      <input onInput={updateValue} value={value} type="text" />
+      <p>Input Value: {value}</p>
+    </div>
+  )
+  newReact.render(element, rootContainer)
+}
+
+rerender('Typed Text')
